5eee9c66e38076e789b4c3aa016af8b650b00c3e
asw357 <wilcoboesveld12@gmail.com>
2025-06-15 15:49:59 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://YOUR-PROJECT-ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR-ANON-KEY';
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
