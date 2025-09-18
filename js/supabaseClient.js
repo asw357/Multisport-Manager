7a22e9b85a8654d4a13d92d43571cbd21e944eda
asw357 <wilcoboesveld12@gmail.com>
2025-09-18 12:33:44 +0200

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,9 +1,7 @@
-<script type="module">
-  import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+// /js/supabaseClient.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-  // Gebruik JOUW projectgegevens (jij gaf deze door)
-  export const supabase = createClient(
-    "https://hvoyctfywbnlkridpenc.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2b3ljdGZ5d2JubGtyaWRwZW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxODEzMTYsImV4cCI6MjA3Mzc1NzMxNn0.n4KHiSBEiVpBcaoPU1XKIvgnK9wvmJuwJoSyw7tdRnI"
-  );
-</script>
+export const supabase = createClient(
+  "https://hvoyctfywbnlkridpenc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2b3ljdGZ5d2JubGtyaWRwZW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxODEzMTYsImV4cCI6MjA3Mzc1NzMxNn0.n4KHiSBEiVpBcaoPU1XKIvgnK9wvmJuwJoSyw7tdRnI"
+);
